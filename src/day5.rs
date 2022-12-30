d32dcac0831c5ff723841f929aa6ec95b1d1971a
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2022-12-30 12:03:22 +0100

    add(day5): working part 1

--- src/day5.rs
+++ src/day5.rs
@@ -5,9 +5,16 @@ use itertools::Itertools;
 use regex::Regex;
 
 struct Action {
-    quantity: u8,
-    from_col: u8,
-    to_col: u8,
+    quantity: usize,
+    from_col: usize,
+    to_col: usize,
+}
+
+#[allow(dead_code)]
+fn print_stacks(stacks: &Vec<Vec<char>>) {
+    stacks.into_iter().enumerate().for_each(|(i, stack)| {
+        println!("{} | {}", i + 1, stack.into_iter().join(" "));
+    })
 }
 
 fn parse_file(filepath: &Path) -> (Vec<Vec<char>>, Vec<Action>) {
@@ -21,7 +28,7 @@ fn parse_file(filepath: &Path) -> (Vec<Vec<char>>, Vec<Action>) {
     // [N] [C]
     // [Z] [M] [P]
     //  1   2   3
-    let mut n_stacks: usize = raw_init_state
+    let n_stacks: usize = raw_init_state
         .split("\n")
         .last()
         .expect(format!("Failed to get last line of {raw_init_state}").as_str())
@@ -37,6 +44,9 @@ fn parse_file(filepath: &Path) -> (Vec<Vec<char>>, Vec<Action>) {
 
     raw_init_state
         .split("\n")
+        .collect_vec()
+        .into_iter()
+        .rev()
         .filter(|&line| line.starts_with("["))
         .for_each(|line| {
             // dbg!(line);
@@ -70,8 +80,8 @@ fn parse_file(filepath: &Path) -> (Vec<Vec<char>>, Vec<Action>) {
             }
             return Action {
                 quantity: caps[1].parse().unwrap(),
-                from_col: caps[2].parse().unwrap(),
-                to_col: caps[3].parse().unwrap(),
+                from_col: caps[2].parse::<usize>().unwrap() - 1,
+                to_col: caps[3].parse::<usize>().unwrap() - 1,
             };
         })
         .collect_vec();
@@ -86,11 +96,21 @@ pub fn run(datapath: &Path, part: u8) {
         filepath.as_os_str().to_str().unwrap()
     );
 
-    let (stacks, actions) = parse_file(&filepath);
+    let (mut stacks, actions) = parse_file(&filepath);
 
-    println!("stacks=\n{stacks:?}")
+    print_stacks(&stacks);
 
-    // actions.into_iter().map(|action| {
+    actions.into_iter().for_each(|action| {
+        let n = stacks[action.from_col].len();
+        let split_at = n.saturating_sub(action.quantity);
+        let head = stacks[action.from_col].split_off(split_at);
+        stacks[action.to_col].extend(head.into_iter().rev());
+    });
 
-    // })
+    let result = stacks
+        .into_iter()
+        .map(|stack| stack.last().expect("Stack is empty").clone())
+        .join("");
+
+    dbg!(&result);
 }
